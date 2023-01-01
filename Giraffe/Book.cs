f06894b2183d161ede62bd10fcb2ce31d8b82bbb
Akash Das <akashdas250901@gmail.com>
2023-01-01 14:33:15 +0530

    third

---
+++ Giraffe/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Book
+    {
+        //adding attributes
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string name)
+        {
+            Console.WriteLine("Book Created "+name);
+        }
+    }
+}
--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -14,26 +14,58 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            //Declaring arrays
-            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
-            string[] friends = new string[2];
-            //Declaring the variables
-            string name = "Akash" + " Das";
-            char n = 'a';
-            float k = 0;//accurate
-            double l = 0.5;//more accurate
-            decimal m = 0; //most accurate
-            int age = 22;
-            bool flag = false;
-            Console.Write("Enter your name ");
-            name = Console.ReadLine();
-            Console.Write("Enter your age ");
-            age = Convert.ToInt32(Console.ReadLine());
-            //Lines of Code
-            Console.WriteLine("Hello " + name); //Console is the window where we can see the output
-            Console.WriteLine("Length " + name.Length);
-            Console.WriteLine("I am " + age + " years old");
-            add(1, 2);
+            // using try catch and finally block for our program
+            try
+            {
+                //Creating an object - an instance of a class
+                Book bk = new Book("AD");
+                bk.title = "Theory";
+                bk.author = "Hey";
+                bk.pages = 225;
+
+                //Declaring arrays
+                int[] arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+                //Declaring a 2D array
+                int[,] arr2 =
+                {
+                    { 2, 3},
+                    { 4, 5}
+                };
+                int[,] arr3 = new int[2, 3]; //2 - rows, 3 coloumns
+                Console.WriteLine(arr2[0, 1]);
+                string[] friends = new string[2];
+                //Declaring the variables
+                string name = "Akash" + " Das";
+                char n = 'a';
+                float k = 0;//accurate
+                double l = 0.5;//more accurate
+                decimal m = 0; //most accurate
+                int age = 22;
+                bool flag = false;
+                Console.Write("Enter your name ");
+                name = Console.ReadLine();
+                Console.Write("Enter your age ");
+                age = Convert.ToInt32(Console.ReadLine());
+                //Lines of Code
+                Console.WriteLine("Hello " + name); //Console is the window where we can see the output
+                Console.WriteLine("Length " + name.Length);
+                Console.WriteLine("I am " + age + " years old");
+                add(1, 2);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                //no matter what this will be executed
+                Console.WriteLine("In Finally Block");
+
+            }
             Console.ReadLine();
         }
         static void add(int a, int b)
